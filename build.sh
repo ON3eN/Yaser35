ac8c8d1504b4cdddcc2146f02257b7427cc6a9bc
ON3eN <goto6946@gmail.com>
2025-07-26 20:14:32 +0300

    add build script

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# سكربت بناء المشروع على Render
-
 echo "📦 تثبيت المتطلبات..."
 pip install -r requirements.txt
 
-echo "🛠️ تنفيذ collectstatic..."
+echo "🛠️ جمع الملفات الثابتة..."
 python manage.py collectstatic --noinput
